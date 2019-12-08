96737ae9392ae7074ffa0dd337a46ea02dee7399
Johnny Ruiz <jruiz@mini.local>
2019-12-07 23:21:25 -0500

    Complete 158. Starting the burger component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -8,27 +8,27 @@ class BurgerIngredient extends Component {
 
     switch(this.props.type) {
     case ('bread-bottom'):
-      ingredient = <div classesName={classes.BreadBottom}></div>;
+      ingredient = <div className={classes.BreadBottom}></div>;
       break;
     case ('bread-top'):
       ingredient = (
-        <div classesName={classes.BreadBottom}>
+        <div className={classes.BreadTop}>
           <div className={classes.Seeds1}></div>
           <div className={classes.Seeds2}></div>
         </div>
       )
       break;
     case ('meat'):
-      ingredient = <div classesName={classes.Meat}></div>;
+      ingredient = <div className={classes.Meat}></div>;
       break;
     case ('cheese'):
-      ingredient = <div classesName={classes.Cheese}></div>;
+      ingredient = <div className={classes.Cheese}></div>;
       break;
     case ('bacon'):
-      ingredient = <div classesName={classes.Bacon}></div>;
+      ingredient = <div className={classes.Bacon}></div>;
       break;
     case ('salad'):
-      ingredient = <div classesName={classes.Salad}></div>;
+      ingredient = <div className={classes.Salad}></div>;
       break;
     default:
       ingredient = null;
