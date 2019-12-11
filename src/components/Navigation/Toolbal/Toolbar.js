e4c7ac38d241e4b45cd28c03fe3de5ba35dd9cac
Johnny Ruiz <jruiz@mini.local>
2019-12-10 20:53:07 -0500

    Complete 178. More about responsive adjustments

--- src/components/Navigation/Toolbal/Toolbar.js
+++ src/components/Navigation/Toolbal/Toolbar.js
@@ -9,7 +9,7 @@ const toolbar = (props) => (
     <div className={classes.Logo}>
       <Logo className={classes.Logo}/>
     </div>
-    <nav>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
